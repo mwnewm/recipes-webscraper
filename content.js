6beaadf00a1d7a492104fdd44703885de0aaad8b
Maeve Newman <maevenewman@gmail.com>
2020-11-08 01:04:18 -0800

    Add react app

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+scrape();
+
+function scrape(){
+  let list = document.getElementsByClassName("ingredients-item");
+  //let list = document.getElementsByTagName("*");
+  console.log("Number of ingredients...")
+  console.log(list.length);
+  var i = 0;
+  while(i < list.length){
+    console.log(list.item(i));
+    i++;
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+
+
+import Button from 'react-bootstrap/Button';
+
+const App = () => (
+  <Container className="p-3">
+    <h1 className="header">Welcome To React-Bootstrap</h1>
+  </Container>
+);
+
+export default App;
